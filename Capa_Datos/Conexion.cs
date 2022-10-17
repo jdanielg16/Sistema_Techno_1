1189a1fe6b62fc2fa4dfec65998535933b342e76
Axel Enrique Linares Velasquez <axel.linares@unah.hn>
2022-10-17 14:06:37 -0700

    Agregar archivos de proyecto.

---
+++ Capa_Datos/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Capa_Datos
+{
+   public class Conexion //Clase Conexion a la base datos
+    {
+        private string Base; //Nombre de la base datos del servidor
+        private string Servidor; // Nombre de nuestro servidor de datos
+        private string UsuarioBD; // Usuario del servidor de datos
+        private string ClaveBD;// Clave del usuario de datos
+        private static Conexion con = null;
+
+        private Conexion()
+        {
+            this.Base="ss";
+        }
+    }
+}
